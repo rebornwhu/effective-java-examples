960a326735a12c6b59dc0f2ee4e4fbacc1526783
rebornwhu <rebornwhu@gmail.com>
2015-09-06 17:58:09 -0700

    Generic types and methods - generic singleton

--- src/main/java/com/xiaolu/item27_generic_methods/SomeMethods.java
+++ src/main/java/com/xiaolu/item27_generic_methods/SomeMethods.java
@@ -3,11 +3,27 @@ package com.xiaolu.item27_generic_methods;
 import java.util.HashSet;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public class SomeMethods {
+
+    public interface UnaryFunction<T> {
+        T apply(T arg);
+    }
 
     public static <E> Set<E> union(Set<E> s1, Set<E> s2) {
         Set<E> result = new HashSet<E>(s1);
         result.addAll(s2);
         return result;
+    }
+
+    private static UnaryFunction<Object> IDENTITY_FUNCTION = new UnaryFunction<Object>() {
+        public Object apply(Object arg) {
+            return arg;
+        }
+    };
+
+    @SuppressWarnings("unchecked")
+    public static <T> UnaryFunction<T> identityFunction() {
+        return (UnaryFunction<T>) IDENTITY_FUNCTION;
     }
 }
--- src/test/java/com/xiaolu/item27_generic_methods/SomeMethodsTest.java
+++ src/test/java/com/xiaolu/item27_generic_methods/SomeMethodsTest.java
@@ -17,4 +17,17 @@ public class SomeMethodsTest {
         Set<String> aflCio = SomeMethods.union(guys, stooges);
         System.out.println(aflCio);
     }
+
+    @Test
+    public void identityFunctionTest() throws Exception {
+        String[] strings = { "jute", "hemp", "nylon" };
+        SomeMethods.UnaryFunction<String> sameString = SomeMethods.identityFunction();
+        for (String s : strings)
+            System.out.println(sameString.apply(s));
+
+        Number[] numbers = { 1, 2.0, 3L };
+        SomeMethods.UnaryFunction<Number> sameNumber = SomeMethods.identityFunction();
+        for (Number n : numbers)
+            System.out.println(sameNumber.apply(n));
+    }
 }
