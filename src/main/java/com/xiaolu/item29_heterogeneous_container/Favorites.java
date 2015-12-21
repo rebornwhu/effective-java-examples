02b240a109f5b4e368a08744239064dd0eb9c31f
rebornwhu <rebornwhu@gmail.com>
2015-12-20 17:08:47 -0800

    Add annotation stuff

--- src/main/java/com/xiaolu/item29_heterogeneous_container/Favorites.java
+++ src/main/java/com/xiaolu/item29_heterogeneous_container/Favorites.java
@@ -1,5 +1,7 @@
 package com.xiaolu.item29_heterogeneous_container;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedElement;
 import java.util.HashMap;
 
 public class Favorites {
@@ -9,11 +11,22 @@ public class Favorites {
     public <T> void putFavorite(Class<T> type, T instance) {
         if (type == null)
             throw new NullPointerException("Type is null");
-        favorites.put(type, instance);
+        favorites.put(type, type.cast(instance));
     }
 
     public <T> T getFavorite(Class<T> type) {
         return type.cast(favorites.get(type));
     }
 
+    static Annotation getAnnotation(AnnotatedElement element, String annotationTypeName) {
+        Class<?> annotationType = null;
+        try {
+            annotationType = Class.forName(annotationTypeName);
+        } catch (Exception ex) {
+            throw new IllegalArgumentException(ex);
+        }
+
+        return element.getAnnotation(
+                annotationType.asSubclass(Annotation.class));
+    }
 }
