227bc0663b6754a0a04dad1ce808a72107ed6d4e
rebornwhu <rebornwhu@gmail.com>
2016-01-18 12:59:59 -0800

    Avoid unnecessary objects

--- src/main/java/com/xiaolu/item5_avoid_unnecessary_objects/Person2.java
+++ src/main/java/com/xiaolu/item5_avoid_unnecessary_objects/Person2.java
@@ -4,25 +4,24 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
 
-public class Person {
-
+public class Person2 {
     private final Date birthDate;
+    private static final Date BOOM_START;
+    private static final Date BOOM_END;
 
-    public Person(Date birthDate) {
+    public Person2(Date birthDate) {
         this.birthDate = birthDate;
     }
 
     public boolean isBabyBoomer() {
-        Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
-        gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
-        Date boomStart = gmtCal.getTime();
-        gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
-        Date boomEnd = gmtCal.getTime();
-
-        return birthDate.compareTo(boomStart) >= 0 && birthDate.compareTo(boomEnd) < 0;
+        return birthDate.compareTo(BOOM_START) >= 0 && birthDate.compareTo(BOOM_END) < 0;
     }
 
-    public Date getBirthDate() {
-        return birthDate;
+    static {
+        Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+        gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
+        BOOM_START = gmtCal.getTime();
+        gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
+        BOOM_END = gmtCal.getTime();
     }
 }
--- src/test/java/com/xiaolu/item5_avoid_unnecessary_objects/PersonTest.java
+++ src/test/java/com/xiaolu/item5_avoid_unnecessary_objects/PersonTest.java
@@ -15,4 +15,21 @@ public class PersonTest {
         }
     }
 
+    @Test
+    public void person2Test() throws Exception {
+        Date[] dates = { new Date(), new Date(), new Date() };
+        for (Date date: dates) {
+            Person2 person = new Person2(date);
+            System.out.println(person.isBabyBoomer());
+        }
+    }
+
+    @Test
+    public void mutateBirthDateTest() throws Exception {
+        Person person = new Person(new Date());
+        Date date1 = person.getBirthDate();
+        System.out.println(date1);
+        person.getBirthDate().setYear(99);
+        System.out.println(date1);
+    }
 }
